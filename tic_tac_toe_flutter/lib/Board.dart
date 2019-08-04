e7457d4528d7209746567f63ce874aefda4bf40f
Lyubomir Vasilev <liubomir1993@gmail.com>
2019-08-04 12:58:27 +0100

    setting up the game rules and the basic UI

---
+++ tic_tac_toe_flutter/lib/Board.dart
@@ -0,0 +1,86 @@
+class Board {
+
+  bool isCross = true;
+  String gameResultMessage;
+  String currentPlayerMessage;
+
+  List<String> boardState;
+
+  static const String emptyState = "empty";
+  static const String crossState = "cross";
+  static const String circleState = "circle";
+
+  makeAMoveAt(int index) {
+    if (this.boardState[index] == emptyState) {
+      if (this.isCross) {
+        this.boardState[index] = crossState;
+      } else {
+        this.boardState[index] = circleState;
+      }
+      this.isCross = !this.isCross;
+      this.evaluateBoard();
+      this.resolveCurrentPlayerText();
+    }
+  }
+
+  resolveCurrentPlayerText() {
+    if (this.isCross) {
+      currentPlayerMessage = "Cross's player turn";
+    } else {
+      currentPlayerMessage = "Circle's player turn";
+    }
+  }
+
+  resetGame() {
+    this.boardState = [
+      emptyState,
+      emptyState,
+      emptyState,
+      emptyState,
+      emptyState,
+      emptyState,
+      emptyState,
+      emptyState,
+      emptyState,
+    ];
+    this.gameResultMessage = "";
+    this.resolveCurrentPlayerText();
+  }
+
+  static const _WIN_CONDITIONS_LIST = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  bool isBoardFull() {
+    for (var val in boardState) {
+      if (val == emptyState) return false;
+    }
+    return true;
+  }
+
+  /// Returns the current state of the board [winning player, draw or no winners yet]
+  String evaluateBoard() {
+    for (var list in _WIN_CONDITIONS_LIST) {
+      if (boardState[list[0]] !=
+          emptyState && // if a player has played here AND
+          boardState[list[0]] ==
+              boardState[list[1]] && // if all three positions are of the same player
+          boardState[list[0]] == boardState[list[2]]) {
+        this.gameResultMessage = '${boardState[list[0]]} wins';
+        return boardState[list[0]];
+      } else {
+        if (isBoardFull()) {
+          this.gameResultMessage = 'The Game is Draw';
+        }
+      }
+    }
+    return emptyState;
+  }
+}
---
+++ tic_tac_toe_flutter/lib/HomePage.dart
@@ -0,0 +1,134 @@
+
+import 'package:flutter/material.dart';
+import 'Board.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  AssetImage cross = AssetImage("images/cross.png");
+  AssetImage circle = AssetImage("images/circle.png");
+  AssetImage edit = AssetImage("images/edit.png");
+  Board board = Board();
+
+  @override
+  void initState() {
+    super.initState();
+    setState(() {
+      board.resetGame();
+    });
+  }
+
+  AssetImage getImage(String value) {
+    switch (value) {
+      case (Board.emptyState):
+        return edit;
+        break;
+      case (Board.crossState):
+        return cross;
+        break;
+      case (Board.circleState):
+        return circle;
+        break;
+    }
+    return null;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Tic Tac Toe",
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        backgroundColor: Color(0xFF192A56),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Expanded(
+            child: GridView.builder(
+              padding: EdgeInsets.all(15.0),
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                  childAspectRatio: 1.0,
+                  crossAxisSpacing: 0.0,
+                  mainAxisSpacing: 0.0),
+              itemCount: board.boardState.length,
+              itemBuilder: (context, i) => GridTile(
+                child: Container(
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.blueGrey, width: 1.0)),
+                  child: Center(
+                    child: MaterialButton(
+                      splashColor: Colors.transparent,
+                      highlightColor: Colors.transparent,
+                      // makes highlight invisible too
+                      onPressed: () {
+                        setState(() {
+                          board.makeAMoveAt(i);
+                        });
+                      },
+                      child: Image(
+                        image: this.getImage(board.boardState[i]),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.all(5.0),
+            child: Text(
+              board.currentPlayerMessage,
+              style: TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.normal,
+              ),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.all(10.0),
+            child: Text(
+              board.gameResultMessage,
+              style: TextStyle(
+                fontSize: 30.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.all(20.0),
+            child: MaterialButton(
+              color: Color(0xFF0A3D62),
+              minWidth: 300.0,
+              height: 50.0,
+              child: Text(
+                "Reset Game",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                ),
+              ),
+              onPressed: () {
+                setState(() {
+                  board.resetGame();
+                });
+              },
+              shape: ContinuousRectangleBorder(
+                borderRadius: BorderRadius.circular(50.0),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
